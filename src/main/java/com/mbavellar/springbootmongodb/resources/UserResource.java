750493d7b57fffe4ec6e5367a7a4b54eee9fde21
Mauricio Avellar <mbavellar@gamil.com>
2022-04-27 23:39:31 -0300

    Using UserDTO to return a Uuser List

--- src/main/java/com/mbavellar/springbootmongodb/resources/UserResource.java
+++ src/main/java/com/mbavellar/springbootmongodb/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.mbavellar.springbootmongodb.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.mbavellar.springbootmongodb.domain.User;
+import com.mbavellar.springbootmongodb.dto.UserDTO;
 import com.mbavellar.springbootmongodb.services.UserService;
 
 @RestController
@@ -19,7 +20,8 @@ public class UserResource {
   private UserService service;
 
   @RequestMapping(method = RequestMethod.GET)
-  public ResponseEntity<List<User>> findAll() {
-    return ResponseEntity.ok().body(service.findAll());
+  public ResponseEntity<List<UserDTO>> findAll() {
+    List<UserDTO> list = service.findAll().stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+    return ResponseEntity.ok().body(list);
   }
 }
